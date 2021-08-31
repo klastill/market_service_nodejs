0ed7fc1f2904c72f1db495e61c00c1cd675581aa
klastill <gyb0501@gmail.com>
2021-08-31 16:57:39 +0900

    add imageSlider on detailPage

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,16 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-
+import ProductImage from './Sections/ProductImage';
+import ProductInfo from './Sections/ProductInfo';
+import {Row, Col} from 'antd';
 
 function DetailProductPage(props) {
 
-  const productId = props.match.params.productId
+  const productId = props.match.params.productId;
+
+  const [Product, setProduct] = useState({});
 
   useEffect(() => {
     axios.get(`/api/product/products_by_id?id=${productId}&type=single`)
       .then(res => {
         if (res.data.success) {
-          console.log(res.data.product);
+          setProduct(res.data.product[0]);
         } else {
           alert('failed to get product');
         }
@@ -18,7 +22,19 @@ function DetailProductPage(props) {
   }, []);
 
   return (
-    <div>
+    <div style={{width: '100%', padding: '3em 4em'}}>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <h1>{Product.productName}</h1>
+      </div>
+      <br />
+      <Row gutter={[16, 16]} >
+        <Col lg={12} sm={24} >
+          <ProductImage detail={Product} />
+        </Col>
+        <Col lg={12} sm={24} >
+          <ProductInfo detail={Product} />
+        </Col>
+      </Row>
       
     </div>
   );
